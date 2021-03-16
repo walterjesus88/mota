e65e9d0f8afa0d3c181524649417fd457b1dc3bd
walter <wjesus88@gmail.com>
2021-03-16 09:01:01 -0500

    lol

--- models/sale_valued.py
+++ models/sale_valued.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models,fields
-
+#from odoo import models,fields,api
+from odoo import api, fields, models, _
 
 #----------------------------------------------------------
 # Stock Picking
 #----------------------------------------------------------
 class order_mota(models.Model):
     _inherit = 'sale.order' 
+
     pedido = fields.Char(string='N de pedido')
     obra = fields.Char(string='Obra/Centro de costos')
     area = fields.Char(string='Frente/area')
@@ -17,10 +18,46 @@ class order_mota(models.Model):
     autorizado = fields.Many2one('res.users', string='Autorizado por', readonly=True, states={'draft': [('readonly', False)], 'completado': [('readonly', False)]}, required=True, change_default=True, index=True, track_visibility='always', track_sequence=1, help="d.")
     recibido = fields.Many2one('res.users', string='Recibido por', readonly=True, states={'draft': [('readonly', False)], 'completado': [('readonly', False)]}, required=True, change_default=True, index=True, track_visibility='always', track_sequence=1, help="Y.")
     solicitante = fields.Many2one('res.users', string='Solicitado por', readonly=True, states={'draft': [('readonly', False)], 'completado': [('readonly', False)]}, required=True, change_default=True, index=True, track_visibility='always', track_sequence=1, help="Yo.") 
+
+    fecha_entrega = fields.Datetime(
+        'Fecha de Entrega Material',
+        default=fields.Datetime.now, index=True, track_visibility='onchange',
+        help="Creation Date")
+
     # state = fields.Selection([
     #     ('inicial','Borrador'),
     #     ('completado', 'completado'),
     #     ], string='Status', readonly=True, copy=False, index=True, tracking=3, default='draft')
+
     
 class order_mota_line(models.Model):
     _inherit = 'sale.order.line' 
+    
+    @api.depends('fecha_retiro','fecha_baja')
+    def _total(self):
+        print(self)
+        print('----------------------------------------------------------------------->')
+        for r in self:
+            print(r.fecha_baja)
+            print(r.fecha_retiro)
+            tiempo_vida = r.fecha_baja - r.fecha_retiro
+            #tiempo_vida = r.tiempo_vida
+            print(tiempo_vida.days)
+            r.tiempo_vida = tiempo_vida.days
+
+    fecha_retiro = fields.Datetime(
+        'Fecha de retiro',
+        default=fields.Datetime.now, index=True, track_visibility='onchange',
+        help="Creation Date")
+    fecha_baja = fields.Datetime(
+        'Fecha de baja',
+        default=fields.Datetime.now, index=True, track_visibility='onchange',
+        help="Creation Date")
+
+    tiempo_vida = fields.Integer(string='Tiempo vida', compute='_total', store=True)
+
+class purchase_guia(models.Model):
+    _inherit = 'purchase.order' 
+    file = fields.Binary(string='Guía de Remisión', attachment=True)
+
+
--- models/stock_valued.py
+++ models/stock_valued.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models,fields
+from odoo import models,fields,api
 
 #----------------------------------------------------------
 # Stock Picking
@@ -13,7 +13,7 @@ class stock_picking(models.Model):
     #numeral = fields.Integer(required=True,string='Guia de Remision de Remitente Nº')
     punto_llegada = fields.Char(string='Domicilio del punto de llegada')
     punto_partida = fields.Char(string='Domicilio del punto de partida')
-    ruc = fields.Char(string='ruc')
+    ruc = fields.Char(string='Ruc')
     destinatario = fields.Char(string='Destinatario')
 
     fecha_traslado = fields.Datetime(
@@ -28,8 +28,20 @@ class stock_picking(models.Model):
     variable = fields.Selection([('weight', 'Weight'), ('volume', 'Volume'), ('wv', 'Weight * Volume'), ('price', 'Price'), ('quantity', 'Quantity')], required=True, default='weight')
     motivo_traslado = fields.Selection([
         ('venta', 'venta'),
+        ('venta sujeta a confirmación de comprador', 'venta sujeta a confirmacion de comprador'),
         ('compra', 'Compra'),
-        ('devolucion', 'Devolucion')]
+        ('consignacion', 'consignacion'),
+        ('devolucion', 'Devolucion'),
+        ('Traslado entre establecimientos de una misma empresa', 'Traslado entre establecimientos de una misma empresa'),
+        ('Traslado de bienes para transformación', 'Traslado de bienes para transformación'),
+        #('Recojo de bienes para transformación', 'Recojo de bienes para transformación'),
+        ('Recojo de bienes transformados', 'Recojo de bienes transformados'),
+        ('Traslado por emisor itinerante de pago', 'Traslado por emisor itinerante de pago'),
+        ('Traslado de zona primaria', 'Traslado de zona primaria'),
+        ('Importación ', 'Importación'),
+        ('Exportación ', 'Exportación'),
+        ('Otros ', 'Otros'),
+        ]
         )
     vehiculo_id = fields.Many2one('fleet.vehicle', 'vehiculos', track_visibility="onchange", help='Driver of the vehicle', copy=False, auto_join=True)
     #outgoing
@@ -43,7 +55,37 @@ class stock_picking(models.Model):
     solicitante = fields.Many2one('res.users', string='Solicitado por', states={'draft': [('readonly', False)], 'completado': [('readonly', False)]},  change_default=True, index=True, track_visibility='always', track_sequence=1, help="Yo.") 
     
     
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
 
+        res = super(stock_picking, self).fields_view_get(
+            view_id=view_id,
+            view_type=view_type,
+            toolbar=toolbar,
+            submenu=submenu)
+        #if res.get('fields').get('state')['selection'][0][1] == 'assigned':      
+
+        a = []
+        if res.get('toolbar', False) and res.get('toolbar').get('print', False):
+            print("www")
+
+            reports = res.get('toolbar').get('print')
+            #for report in reports:
+                #print(res['toolbar']['print'])   
+                #if report.get('report_file', False) and report.get('report_file') == 'stock.report_deliveryslip':
+                #    res['toolbar']['print'].remove(report)              
+                #if report.get('report_file') == 'stock.label_transfer_template_view_pdf' and report.get('report_file') == 'stock.label_transfer_template_view_zpl':
+                #    res['toolbar']['print'].remove(report)
+             
+                #if report.get('report_file') == 'stock.label_transfer_template_view_zpl':
+                #if report.get('print_report_name') == False:
+                    #print_report_name
+
+                    #res['toolbar']['print'].remove(report)
+                #a.append(res)
+                #print(res)
+            #a.append(res)
+        return res
 class VehicleMota(models.Model):
     _inherit = 'fleet.vehicle'
     _description = 'Vehicle mota'
